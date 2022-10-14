8abf11393ab6683797b9d8d5b336409bc4f0d4b3
treyturley <81778542+treyturley@users.noreply.github.com>
2022-10-14 15:09:13 -0500

    initial commit

---
+++ src/components/FSEJobFinder.js
@@ -0,0 +1,9 @@
+// NOTE: import css here when we define css for this component specifically
+
+function FSEJobFinder() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default FSEJobFinder;
