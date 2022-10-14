8d1e4665735b1427c7934682ec570883c3b2dada
treyturley <81778542+treyturley@users.noreply.github.com>
2022-10-14 15:18:11 -0500

    adds styles folder and moves index styles into it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import './styles/index.css';
 
 import FSEJobFinder from './components/FSEJobFinder';
 
