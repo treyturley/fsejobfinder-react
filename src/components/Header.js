270c903f16b453a3fd5d0acf676a174e0bbe7ea5
treyturley <81778542+treyturley@users.noreply.github.com>
2022-10-25 13:54:29 -0500

    implements header with set user key button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import "../styles/Header.css"
 
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function Header() {
+
+  const [userKey, setUserKey] = useState("");
+  const [isUserKeySet, setIsUserKeySet] = useState(false);
+
+  // TODO: handle case where user key already exists in session
+
+  const handleAccessKeyChange = event => {
+    setUserKey(event.target.value);
+  }
+
+  function setAccessKey() {
+    if (userKey.length > 0) {
+      sessionStorage.setItem("user-key", userKey);
+      setIsUserKeySet(true);
+    }
+  }
+
+  function resetAccessKey() {
+    setUserKey("");
+    sessionStorage.removeItem("user-key");
+  }
+
+  //TODO: if user key is set, remove input box and button and
+  //      add text indicating the key is set
+
+  const sessionUserKey = sessionStorage.getItem("user-key");
+  if (sessionUserKey && !isUserKeySet) {
+    setUserKey(sessionUserKey);
+    setIsUserKeySet(true);
+  }
+
   return (
     <>
       <Navbar bg="primary" variant="dark" className="header">
         <Container>
           <Navbar.Brand href="#">FSE Job Finder</Navbar.Brand>
+          {!isUserKeySet &&
+            <Form className="d-flex">
+              <Form.Control
+                type="text"
+                placeholder="User Access Key"
+                className="me-2"
+                aria-label="User Access Key"
+                value={userKey || ""}
+                onChange={handleAccessKeyChange}
+              />
+              <Button
+                variant="light"
+                size="sm"
+                className='no-wrap'
+                onClick={setAccessKey}>
+                Set Access Key
+              </Button>
+            </Form>
+          }
+          {isUserKeySet &&
+            <>
+              <Button
+                variant="light"
+                size="sm"
+                className='no-wrap'
+                onClick={() => {
+                  resetAccessKey();
+                  setIsUserKeySet(false);
+                }}>
+                Reset Access Key
+              </Button>
+            </>
+          }
         </Container>
       </Navbar>
     </>
